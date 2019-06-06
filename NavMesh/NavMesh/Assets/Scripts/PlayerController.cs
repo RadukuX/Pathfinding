c919cad448c87fbf24dcacc99990ab4122ef1ea9
RadukuX <41450735+RadukuX@users.noreply.github.com>
2019-06-06 13:30:03 +0300

    Initial commit

---
+++ NavMesh/NavMesh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public NavMeshAgent navMeshAgent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            //Input.mousePosition
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
